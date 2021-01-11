5961360f91333557d099ae087ca1ebf55afb3b4f
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-01-11 16:25:12 -0500

    commit tree 2b198ad1415816eddadbcdfa8ce0b63b5b344766

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,4 @@
+class Costume < ActiveRecord::Base 
+end 
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,3 @@
+class HauntedHouse < ActiveRecord::Base 
+end 
 # Create your HauntedHouse class here
